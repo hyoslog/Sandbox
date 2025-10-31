9ada70c86d7d349176ca91c4327d32a12c6c0a1d
강효진 <98869559+hyoslog@users.noreply.github.com>
2025-10-31 17:15:03 +0900

    프로젝트 생성
    - C++ ThirdPerson Template
    - Quality Preset: Scalable

---
+++ Source/Sandbox/Sandbox.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Sandbox : ModuleRules
+{
+	public Sandbox(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Sandbox",
+			"Sandbox/Variant_Platforming",
+			"Sandbox/Variant_Platforming/Animation",
+			"Sandbox/Variant_Combat",
+			"Sandbox/Variant_Combat/AI",
+			"Sandbox/Variant_Combat/Animation",
+			"Sandbox/Variant_Combat/Gameplay",
+			"Sandbox/Variant_Combat/Interfaces",
+			"Sandbox/Variant_Combat/UI",
+			"Sandbox/Variant_SideScrolling",
+			"Sandbox/Variant_SideScrolling/AI",
+			"Sandbox/Variant_SideScrolling/Gameplay",
+			"Sandbox/Variant_SideScrolling/Interfaces",
+			"Sandbox/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
